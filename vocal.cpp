4420107616ca6cc6df7609e1d1b8848ffc487a37
mariasambrano <143769842+mariasambrano@users.noreply.github.com>
2023-11-10 18:33:56 -0600

    Add files via upload

---
+++ vocal.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+int main(){
+	char letra;
+	cout<<"digita una letra ";
+	cin>>letra;
+	if(letra == 'a' or letra == 'e' or letra == 'i' or letra == 'o'  or letra == 'u')
+    cout<<"es vocal";
+    else
+    cout<<"no es vocal";
+    
+    return 0;
+		}
